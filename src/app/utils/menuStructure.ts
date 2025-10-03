fe6c044b9b4810e4377dc627e7a3bf1aaedccac4
matheus-henrique-skyline <matheussantos.skyline@gmail.com>
2025-10-03 09:42:05 -0300

    Initial commit

---
+++ src/app/utils/menuStructure.ts
@@ -0,0 +1,46 @@
+const menuStructure = [
+  {
+    title: "Vídeos",
+    submenu: ["Vídeo 1", "Vídeo 2", "Vídeo 3"],
+    caminho: "/project-name/videos",
+    submenuElements: ["video-1", "video-2", "video-3"],
+  },
+  {
+    title: "Imagens",
+    submenu: [""],
+    caminho: "/project-name/imagens",
+    submenuElements: [""],
+  },
+  {
+    title: "Implantação",
+    submenu: ["IMPLANTAÇÃO", "COMPARE"],
+    caminho: "/project-name/implantacao",
+    submenuElements: ["implantacao", "compare"],
+  },
+  {
+    title: "Plantas",
+    submenu: [""],
+    caminho: "/project-name/plantas",
+    submenuElements: [""],
+  },
+  {
+    title: "Localização",
+    submenu: ["ULTRATOUR", "MAPAS"],
+    caminho: "/project-name/localizacao",
+    submenuElements: ["ultratour", "mapa-2d"],
+  },
+  {
+    title: "Ficha Técnica",
+    submenu: [""],
+    caminho: "/project-name/ficha-tecnica",
+    submenuElements: [""],
+  },
+  {
+    title: "Institucional",
+    submenu: [""],
+    caminho: "/project-name/institucional",
+    submenuElements: [""],
+  },
+];
+
+export default menuStructure;
