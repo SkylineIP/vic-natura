dfd38880a37a54f14507d49b093bb1d328ff8590
Matheus Santos <matheussantos.skyline@gmail.com>
2025-10-16 14:42:43 -0300

    -fix add public folder

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false, // Desativa Strict Mode
+  output: "export",
   images: {
     unoptimized: true,
   },
